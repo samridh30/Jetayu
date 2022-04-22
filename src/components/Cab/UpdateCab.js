43121465d35cdcab1d8866a37e42e962c1b7e7bf
samridh30 <97982455+samridh30@users.noreply.github.com>
2022-04-22 14:23:58 +0530

    cab_Component

---
+++ src/components/Cab/UpdateCab.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const UpdateCab = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default UpdateCab
---
+++ src/components/Cab/ViewAllCabs.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const ViewAllCabs = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default ViewAllCabs
