4a2d348c97a14bbc78b788c350f9762b08959ffa
Sajal Ganjewala <sg748@snu.edu.in>
2022-04-29 09:20:59 +0530

    Login, Register Styles fixed

--- src/components/Auth/Register.js
+++ src/components/Auth/Register.js
@@ -34,82 +34,87 @@ const Register = () => {
         event.preventDefault();
     }
     return (
-        <div className="container">
-            <div className="col-4 mt-3 pb-3 shadow bg-white" >
-                <h1 className="display-4 text-primary">Register</h1>
-                <form className="form form-group form-dark " onSubmit={submitAppUser}>
-                    <div>
-                        <input
-                            type="text"
-                            name="userName"
-                            id="userName"
-                            className="form-control mb-3"
-                            placeholder="Enter username"
-                            value={appUser.userName}
-                            onChange={handleAppUser}
-                            required
-                            autoFocus
-                        />
-                        <input
-                            type="email"
-                            name="email"
-                            id="email"
-                            className="form-control mb-3"
-                            placeholder="Enter Email"
-                            value={appUser.email}
-                            onChange={handleAppUser} />
-                        <input
-                            type="password"
-                            name="password"
-                            id="password"
-                            className="form-control mb-3"
-                            placeholder="Enter password"
-                            value={appUser.password}
-                            onChange={handleAppUser} />
+        <center>
 
-                        <input
-                            type='number'
-                            name="phone"
-                            id="phone"
-                            className="form-control mb-3"
-                            placeholder="Enter Phone number"
-                            value={appUser.mobileNumber}
-                            onChange={handleAppUser} />
-                        <input
-                            type='text'
-                            name="Address"
-                            id="address"
-                            className="form-control mb-3"
-                            placeholder="Enter Address"
-                            value={appUser.address}
-                            onChange={handleAppUser} />
+            <div className="container">
+                <div className="w-50 shadow bg-white" style={{ borderRadius: "10px" }} >
+                    <form className="p-3 justify-content-center mt-5 form form-group form-dark" onSubmit={submitAppUser}>
+                        <h1 className="display-4 text-primary">Register</h1>
+                        <div className='p-3'>
+                            <input
+                                type="text"
+                                name="userName"
+                                id="userName"
+                                className="form-control mb-3"
+                                placeholder="Enter username"
+                                value={appUser.userName}
+                                onChange={handleAppUser}
+                                required
+                                autoFocus
+                            />
+                            <input
+                                type="email"
+                                name="email"
+                                id="email"
+                                className="form-control mb-3"
+                                placeholder="Enter Email"
+                                value={appUser.email}
+                                onChange={handleAppUser} />
+                            <input
+                                type="password"
+                                name="password"
+                                id="password"
+                                className="form-control mb-3"
+                                placeholder="Enter password"
+                                value={appUser.password}
+                                onChange={handleAppUser} />
+
+                            <input
+                                type='number'
+                                name="phone"
+                                id="phone"
+                                className="form-control mb-3"
+                                placeholder="Enter Phone number"
+                                value={appUser.mobileNumber}
+                                onChange={handleAppUser} />
+                            <input
+                                type='text'
+                                name="Address"
+                                id="address"
+                                className="form-control mb-3"
+                                placeholder="Enter Address"
+                                value={appUser.address}
+                                onChange={handleAppUser} />
 
 
 
-                        <div className="form-group">
-                            <select className="form-control mb-3" name="role" id="role" onChange={handleAppUser}>
-                                <option value="Role">Select a role</option>
-                                <option value="ADMIN">Admin</option>
-                                <option value="CUSTOMER">Customer</option>
-                            </select>
+                            <div className="form-group">
+                                <select className="form-control mb-3" name="role" id="role" onChange={handleAppUser}>
+                                    <option className='text-muted' value="Role">Select a role</option>
+                                    <option value="ADMIN">Admin</option>
+                                    <option value="CUSTOMER">Customer</option>
+                                </select>
+                            </div>
+                            <input
+                                type="submit"
+                                id="submit"
+                                name="submit"
+                                className="w-75 form-control btn btn-success mb-3"
+                                value="Register"
+                                onClick={submitAppUser}
+                            />
                         </div>
-                        <input
-                            type="submit"
-                            id="submit"
-                            name="submit"
-                            className="form-control btn btn-success mb-3"
-                            value="Register"
-                            onClick={submitAppUser}
-                        />
-                    </div>
-                </form>
-                <p className="text-danger">{credentials}</p>
-                <Link to="/login" className="btn btn-primary col-12">Already registered? Login</Link>
-            </div>
-            <div>
-                {/* https://material.io/components/dialogs/web#alert-dialog */}
-            </div>
-        </div >
+                    </form>
+                    <p className="text-danger">{credentials}</p>
+                    <p className='font-weight-lighter'>
+                        Already a Member? <Link to="/login">Login</Link>
+                    </p>
+                </div>
+                <div>
+                    {/* https://material.io/components/dialogs/web#alert-dialog */}
+                </div>
+            </div >
+        </center>
 
     )
 }
