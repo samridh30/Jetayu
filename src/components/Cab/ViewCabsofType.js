952216221fae134d4663cf6b1afe0d62f94fdc18
Srikanth@946 <ghattamanenisrikanth8@gmail.com>
2022-04-29 11:36:59 +0530

    cab update

--- src/components/Cab/ViewCabsofType.js
+++ src/components/Cab/ViewCabsofType.js
@@ -1,24 +1,21 @@
 import React, { useState } from "react";
-import {  viewCabsofType } from "../../services/CabService";
-import { useDispatch, useSelector } from "react-redux";
+import { viewCabsofType } from "../../services/CabService";
 
 const ViewCabsofType = () => {
   const [type, settype] = useState("");
   const [cabTypeData, setcabTypeData] = useState([]);
-  const dispatch = useDispatch();
 
   const handletripTypeData = (e) => {
-    console.log(e.target.value)
+    console.log(e.target.value);
     settype(e.target.value);
   };
 
   const GetCabsByType = (evt) => {
-    console.log(type)
+    console.log(type);
     evt.preventDefault();
     viewCabsofType(type)
       .then((response) => {
-        // dispatch(setAllTripsList(response.data));
-        console.log(response.data)
+        console.log(response.data);
         setcabTypeData(response.data);
       })
       .catch((error) => {
@@ -29,46 +26,54 @@ const ViewCabsofType = () => {
   return (
     <div>
       <div>
-        <label className="text-drak"><h3>Enter Cab Type</h3></label>
-        <input
-          type="text"
-          placeholder="Enter Cab Type"
-          className=" form-control col-md-3 px-2 w-100 m-auto p-3 mb-3 "
+        <label className="text-drak">
+          <h3>Enter Cab Type</h3>
+        </label>
+        <select
+          className="form-select col-md-3 px-2 w-100 m-auto mb-2"
           value={type}
           onChange={handletripTypeData}
-        />
+          name="cabType"
+          placeholder="CabType"
+        >
+          <option value="" selected>
+            Choose Cab Type...
+          </option>
+          <option value="Mini">Mini</option>
+          <option value="Auto">Auto</option>
+          <option value="Luxury">Luxury</option>
+        </select>
         <input
           type="submit"
           placeholder="Get Cabs"
           className="btn-success col-md-3 px-2 w-100 m-auto mb-3"
-          value='Get Cab'
+          value="Get Cab"
           onClick={GetCabsByType}
         />
       </div>
-      {cabTypeData.length>=1 &&
-      <table className="table table-bordered table-light table-striped col-lg-6 m-auto">
-        <thead className="thead-dark">
-          <tr>
-            <th>cabId</th>
-            <th>carType</th>
-            <th>perKmRate</th>
-            <th>status</th>
-          </tr>
-        </thead>
-        <tbody>
-          {cabTypeData.map((e, index) => (
-            <tr key={index}>
-              <td>{e.cabId}</td>
-              <td>{e.carType}</td>
-              <td>{e.perKmRate}</td>
-              <td>{e.status.toString()}</td>
+      {cabTypeData.length >= 1 && (
+        <table className="table table-bordered table-light table-striped col-lg-6 m-auto">
+          <thead className="thead-dark">
+            <tr>
+              <th>cabId</th>
+              <th>carType</th>
+              <th>perKmRate</th>
+              <th>status</th>
             </tr>
-          ))}
-        </tbody>
-      </table>
-      }
+          </thead>
+          <tbody>
+            {cabTypeData.map((e, index) => (
+              <tr key={index}>
+                <td>{e.cabId}</td>
+                <td>{e.carType}</td>
+                <td>{e.perKmRate}</td>
+                <td>{e.status.toString()}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      )}
     </div>
-          
   );
 };
 
