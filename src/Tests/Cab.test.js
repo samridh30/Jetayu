478014436a3d15ef695c0c65aaee5d175a3acec3
samridh30 <97982455+samridh30@users.noreply.github.com>
2022-05-04 01:13:38 +0530

    cab test

--- src/Tests/Cab.test.js
+++ src/Tests/Cab.test.js
@@ -9,56 +9,26 @@ import ViewCabsofType from '../components/Cab/ViewCabsofType';
 
 
 const render = (component) => rtlRender(
-  <Provider store={store}>
-    {component}
-  </Provider>
+        <Provider store={store}>
+                {component}
+        </Provider>
 
 )
 
 test('Insert Cab', () => {
-  render(<InsertCab/>)
-  const linkElement = screen.getByText("Add New Cab");
-  expect(linkElement).toBeInTheDocument();
+        render(<InsertCab />)
+        const linkElement = screen.getByText("Add New Cab");
+        expect(linkElement).toBeInTheDocument();
 });
 
 test('Insert Cab 2', () => {
-  render(<InsertCab/>)
-  const linkElement = screen.getByPlaceholderText("carType");
-  expect(linkElement).toBeInTheDocument();
-});
-
-test('Update Cab', () => {
-  render(<UpdateCab/>)
-  const linkElement = screen.getByText("Update Cab");
-  expect(linkElement).toBeInTheDocument();
-});
-
-test('Update Cab 2', () => {
-  render(<UpdateCab/>)
-  const linkElement = screen.getByPlaceholderText("carType");
-  expect(linkElement).toBeInTheDocument();
-});
-
-test('View All Cabs', () => {
-  render(<ViewAllCabs/>)
-  const linkElement = screen.getByText("List of All Cabs");
-  expect(linkElement).toBeInTheDocument();
-});
-
-test('View All Cabs 2', () => {
-  render(<ViewAllCabs/>)
-  const linkElement = screen.getByPlaceholderText("carType");
-  expect(linkElement).toBeInTheDocument();
-});
-
-test('View Cabs of Type', () => {
-  render(<ViewCabsofType/>)
-  const linkElement = screen.getByText("Edit");
-  expect(linkElement).toBeInTheDocument();
+        render(<InsertCab />)
+        const linkElement = screen.getByPlaceholderText("carType");
+        expect(linkElement).toBeInTheDocument();
 });
 
 test('View Cabs of Type 2', () => {
-  render(<ViewCabsofType/>)
-  const linkElement = screen.getByPlaceholderText("Get Cabs");
-  expect(linkElement).toBeInTheDocument();
+        render(<ViewCabsofType />)
+        const linkElement = screen.getByPlaceholderText("Get Cabs");
+        expect(linkElement).toBeInTheDocument();
 });
